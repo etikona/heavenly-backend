1c46caec1a0c70a88dea80b7f40e7225c7414c87
eti kona <etikonapal@gmail.com>
2026-06-02 22:18:30 +0600

    cors update

--- app.js
+++ app.js
@@ -24,7 +24,13 @@ dotenv.config();
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   } */
-app.use(cors());
+app.use(
+  cors({
+    origin: "*" || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
